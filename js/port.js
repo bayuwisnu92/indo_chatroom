f4e068fa0fa7b7b78fe41ce41f75849b2b679a55
bayuwisnu92 <bayuwisnu9294@gmail.com>
2026-04-29 11:43:33 +0700

    link anyar deui

--- js/port.js
+++ js/port.js
@@ -1,4 +1,4 @@
-export const port = 'https://transparency-bristol-marcus-difficulties.trycloudflare.com'
+export const port = 'https://killing-heath-cope-roles.trycloudflare.com'
 
 
 // 'http://localhost:3000'
